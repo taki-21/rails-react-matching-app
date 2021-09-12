6fdef39060ec025159e25ae9bd5e44c33c002817
taki-21 <taki.it.0208.21@gmail.com>
2021-09-13 01:38:50 +0900

    Add: Viewの追加

--- frontend/src/interfaces/index.ts
+++ frontend/src/interfaces/index.ts
@@ -1,9 +1,17 @@
-//サインアップ
+// サインアップ
 export interface SignUpData {
   name: string
   email: string
   password: string
   passwordConfirmation: string
+  gender: number
+  prefecture: number
+  birthday: Date
+  image: string
+}
+
+export interface SignUpFormData extends FormData {
+  append(name: keyof SignUpData, value: String | Blob, fileName?: string): any
 }
 
 // サインイン
@@ -19,8 +27,50 @@ export interface User {
   provider: string
   email: string
   name: string
-  nickname?: string
-  image?: string
+  image: {
+    url: string
+  }
+  gender: number
+  birthday: String | number | Date
+  profile: string
+  prefecture: number
   allowPasswordChange: boolean
+  createdAt?: Date
+  updatedAt?: Date
 }
 
+export interface UpdateUserData {
+  id: number | undefined | null
+  name?: string
+  prefecture?: number
+  profile?: string
+  image?: string
+}
+
+export interface UpdateUserFormData extends FormData {
+  append(name: keyof UpdateUserData, value: String | Blob, fileName?: string): any
+}
+
+// いいね
+export interface Like {
+  id?: number
+  fromUserId: number | undefined | null
+  toUserId: number | undefined | null
+}
+
+// チャットルーム
+export interface ChatRoom {
+  chatRoom: {
+    id: number
+  }
+  otherUser: User,
+  lastMessage: Message
+}
+
+// メッセージ
+export interface Message {
+  chatRoomId: number
+  userId: number | undefined
+  content: string
+  createdAt?: Date
+}
--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -1,19 +1,19 @@
 import client from "lib/api/client"
 import Cookies from "js-cookie"
 
-import { SignUpData, SignInData } from "interfaces/index"
+import { SignUpFormData, SignInData } from "interfaces/index"
 
-// サインアップ（新規アカウント作成）
-export const signUp = (data: SignUpData) => {
+// サインアップ
+export const signUp = (data: SignUpFormData) => {
   return client.post("auth", data)
 }
 
-// サインイン（ログイン）
+// サインイン
 export const signIn = (data: SignInData)  => {
   return client.post("auth/sign_in", data)
 }
 
-// サインアウト（ログアウト）
+// サインアウト
 export const signOut = () => {
   return client.delete("auth/sign_out", { headers: {
     "access-token": Cookies.get("_access_token"),
@@ -22,10 +22,10 @@ export const signOut = () => {
   }})
 }
 
-// 認証済みのユーザーを取得
+// 認証中ユーザーの情報を取得
 export const getCurrentUser = () => {
   if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
-  return client.get("/auth/sessions", { headers: {
+  return client.get("auth/sessions", { headers: {
     "access-token": Cookies.get("_access_token"),
     "client": Cookies.get("_client"),
     "uid": Cookies.get("_uid")
